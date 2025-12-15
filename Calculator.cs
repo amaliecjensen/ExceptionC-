6d875d4783cb97ad67ea39fafe3f472927b6ad50
amaliecjensen <amaliecolstrup@icloud.com>
2025-12-15 19:25:46 +0100

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,7 @@
+public class Calculator
+{
+    public int Divide(int numerator, int denomenator)
+    {
+        return numerator / denomenator;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        var streamReader = new StreamReader("heeej");
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine("an error occured, exception caught");
+        }
+        finally //will always be exc
+        {
+            System.Console.WriteLine("finally block excecuted");
+            streamReader.Dispose();
+        }
+
+    }
+
+}
